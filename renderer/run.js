b4e09bc9d052d2d1272b8f404ea2c97c60f7f887
Phil Boardman <pboardman@gmail.com>
2015-11-19 22:24:21 -0600

    added error logging to see compile errors in tests

--- renderer/run.js
+++ renderer/run.js
@@ -5,11 +5,14 @@ var mocha = require('../mocha')
 // Expose mocha
 window.mocha = require('mocha')
 
-// consider hooking up to mocha
-/* window.onerror = function (message, filename, lineno, colno, err) {
-  console.log(err.message)
-  console.log(err.stack)
-}*/
+window.onerror = function (message, filename, lineno, colno, err) {
+  ipc.send('mocha-error', {
+    message: message,
+    filename: filename,
+    err: err,
+    stack: err.stack
+  })
+}
 
 // console.log(JSON.stringify(window.__args__, null, 2))
 mocha.run(window.__args__, function (failureCount) {
