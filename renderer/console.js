7a333132f17d3158b9185c04c7b46017eaa8b318
Ming Luo <mcluo1994@gmail.com>
2015-11-21 13:59:31 -0500

    Check electron version to require correctly
    This gets rid of a deprecation warning upon use

--- renderer/console.js
+++ renderer/console.js
@@ -1,4 +1,12 @@
-var remote = require('remote')
+var remote
+// Check if electron version is >0.35.0
+var electronV = process.versions['electron'].split('.')
+if (parseInt(electronV[1], 10) >= 35 && electronV[0] === '0') {
+  var electron = require('electron')
+  remote = electron.remote
+} else {
+  remote = require('remote')
+}
 var remoteConsole = remote.require('console')
 
 // we have to do this so that mocha output doesn't look like shit
--- renderer/run.js
+++ renderer/run.js
@@ -1,6 +1,14 @@
 require('./console')
-var ipc = require('ipc')
 var mocha = require('../mocha')
+var ipc
+// Check if electron version is >0.35.0
+var electronV = process.versions['electron'].split('.')
+if (parseInt(electronV[1], 10) >= 35 && electronV[0] === '0') {
+  var electron = require('electron')
+  ipc = electron.ipcMain
+} else {
+  ipc = require('ipc')
+}
 
 // Expose mocha
 window.mocha = require('mocha')
