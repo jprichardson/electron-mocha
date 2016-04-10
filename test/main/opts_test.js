d1c926344fc90001a8e05067353714cc6f0c2392
Sylvester Keil <sylvester@keil.or.at>
2016-04-10 18:00:44 +0200

    Linting

--- test/main/opts_test.js
+++ test/main/opts_test.js
@@ -1,5 +1,7 @@
 var assert = require('assert')
 
+/* global describe it */
+
 describe('mocha.opts', function () {
   it('--require modules are loaded', function () {
     assert.equal(true, global.required)
