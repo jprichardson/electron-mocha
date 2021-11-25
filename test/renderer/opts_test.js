1c456a1c45148da1002321a8fb41ad3587fb4c3c
Sylvester Keil <sylvester@keil.or.at>
2021-11-25 14:14:11 +0100

    Use ipc helper instead of remote module

--- test/renderer/opts_test.js
+++ test/renderer/opts_test.js
@@ -1,27 +1,27 @@
 'use strict'
 
 const assert = require('assert')
-const remote = require('electron').remote
+const { ipcRenderer } = require('electron')
 
 describe('mocha.opts', () => {
   it('--require modules are loaded in renderer', () => {
     assert.strictEqual(true, window.required)
   })
 
-  it('--require modules are not loaded in main', () => {
-    assert.strictEqual(undefined, remote.getGlobal('required'))
+  it('--require modules are not loaded in main', async () => {
+    assert.strictEqual(undefined, await ipcRenderer.invoke('get-global', 'required'))
   })
 
   it('--script modules are loaded', () => {
     assert.strictEqual(true, window.preloaded)
   })
 
-  it('--require-main modules are loaded in the main process', () => {
-    assert.strictEqual(true, remote.getGlobal('requiredMain'))
+  it('--require-main modules are loaded in the main process', async () => {
+    assert.strictEqual(true, await ipcRenderer.invoke('get-global', 'requiredMain'))
   })
 
-  it('--require-main modules are loaded before "ready"', () => {
-    assert.strictEqual(true, remote.getGlobal('requiredMainBeforeReady'))
+  it('--require-main modules are loaded before "ready"', async () => {
+    assert.strictEqual(true, await ipcRenderer.invoke('get-global', 'requiredMainBeforeReady'))
   })
 
   it('--url custom page loaded', () => {
